908b31a24d4f9c20d3a3668fcab887e3186f9cbf
CodegassW <wchweichenhao@gmail.com>
2025-05-30 08:41:03 -0400

    update interface

--- src/aaa_issue_scanner/__main__.py
+++ src/aaa_issue_scanner/__main__.py
@@ -4,7 +4,7 @@ AAA Issue Scanner main entry point
 Can be run via python -m aaa_issue_scanner
 """
 
-from .cli import main
+from .cli import cli
 
 if __name__ == '__main__':
-    main() 
+    cli() 
